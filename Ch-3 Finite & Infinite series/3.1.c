12903f5def210d0a66f329668acc96f5a8b5fecb
Prateek <129680335+Prateek-Physics@users.noreply.github.com>
2025-12-17 11:30:37 +0530

    Create 3.1.c

---
+++ Ch-3 Finite & Infinite series/3.1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{float i,s=1,t,x=1.5; // for x=1.5
+  for(i=2;i<20;i++)
+    {s+=(1/(pow(x,i)));}
+  printf("sum is %f\n",s);
+
+}
+// matches with solution calculated by gp series
